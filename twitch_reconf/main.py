a304076123cd61de6597e7768de603a6fe9b1826
Justin Hammond <justin.hammond@rackspace.com>
2018-10-23 06:18:47 -0500

    Added html files for realtime refreshing

--- twitch_reconf/main.py
+++ twitch_reconf/main.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 
 import click
+import jinja2 as j2
 
 import config
 import constants
@@ -13,24 +14,28 @@ import constants
 @click.option('--debug', is_flag=True,
               help='Debug mode flag for development mode. '
               'Sets logging to debug level')
-@click.argument('source_key')
-def run(debug, source_key):
+@click.option('--check', is_flag=True)
+@click.argument('game')
+def run(debug, check, game):
     conf = config.Configuration()
+    for d in conf._get_directory_info():
+        print d
     mapping = {}
+    list = False
+    if game == 'list':
+        list = True
     print "=" * 75
 
     print 'Extension Mappings:'
     for k, v in conf.config.get('TWRECONF_EXT', {}).iteritems():
-        if k == 'dir':
-            continue
         mapping[k] = v
-        print "%s = %s" % (k, v)
+        print "*.%s = %s" % (k, v)
     dir_map = conf.config.get('TWRECONF', {}).get('dir')
     print "Directory Mapping:"
     print "%s = %s" % ('directory', 'dir_map')
 
-    source = conf.config.get('TWSOURCE', {}).get(source_key)
     target = conf.config.get('TWRECONF', {}).get('target')
+    source = conf.config.get('TWRECONF', {}).get('source')
     if source is None or target is None:
         print 'Source or target is invalid'
         exit(1)
@@ -43,25 +48,95 @@ def run(debug, source_key):
         print 'Target not a directory (%s)' % target
         exit(1)
 
+
+    slideshow_dir_src = os.path.join(constants.PROJECT_ROOT, 'slideshow')
+    template_dir = os.path.join(
+        constants.PROJECT_ROOT, 'template')
+    slideshow_html_src = os.path.join(
+        constants.PROJECT_ROOT, 'template', 'slideshow.j2.html')
+    logo_html_src = os.path.join(
+        constants.PROJECT_ROOT, 'template', 'logo.j2.html')
+    video_html_src = os.path.join(
+        constants.PROJECT_ROOT, 'template', 'video.j2.html')
+    slideshow_imgdir = os.path.join(target, dir_map, 'img')
+    slideshow_html_file = os.path.join(target, dir_map, 'slideshow.html')
+    logo_html_file = os.path.join(target, 'logo.html')
+    video_html_file = os.path.join(target, 'video.html')
+
+    template = None
+    with open(slideshow_html_src, 'r') as file:
+        template = j2.Template(file.read())
+
+    logo_template = None
+    with open(logo_html_src, 'r') as file:
+        logo_template = j2.Template(file.read())
+
+    video_template = None
+    with open(video_html_src, 'r') as file:
+        video_template = j2.Template(file.read())
+
     print "-" * 75
-    files = glob.glob(os.path.join(target, '*'))
-    for f in files:
-        if os.path.isdir(f):
-            shutil.rmtree(f)
-            print 'Removed %s' % str(f)
-        else:
-            os.remove(f)
-            print 'Removed %s' % str(f)
+    if not check and not list:
+        delfiles = glob.glob(os.path.join(target, '*'))
+        for delf in delfiles:
+            if os.path.isdir(delf):
+                shutil.rmtree(delf)
+                print 'Removed %s' % str(delf)
+            else:
+                os.remove(delf)
+                print 'Removed %s' % str(delf)
 
-    files = glob.glob(os.path.join(source, '*'))
     print "+" * 75
-    for f in files:
-        if os.path.isdir(f):
-            shutil.copytree(f, os.path.join(target, dir_map))
-            print 'Copied %s to %s' % (str(f), dir_map)
-        else:
-            for k, v in mapping.iteritems():
-                if str(f).endswith(k):
-                    shutil.copy(f, os.path.join(target, v))
-                    print 'Copied %s to %s' % (str(f), os.path.join(target, v))
+    files = glob.glob(os.path.join(source, '*'))
+    if list:
+        game_list = [os.path.basename(f) for f in files]
+        print ', '.join(game_list)
+        exit(0)
 
+    for subdir in files:
+        hasdir = False
+        haskey = {k: False for k in mapping.keys()}
+        if(
+                os.path.isdir(subdir) and 
+                (os.path.basename(subdir) == game or check)):
+            if 'Unsorted' == str(os.path.basename(subdir)):
+                continue
+
+            gamefiles = glob.glob(os.path.join(subdir, '*'))
+            for gfile in gamefiles:
+                if os.path.isdir(gfile):
+                    slide_dir = os.path.basename(gfile)
+                    hasdir = True
+                    if not check:
+                        shutil.copytree(
+                            slideshow_dir_src,
+                            os.path.join(target, dir_map))
+                        images = glob.glob(os.path.join(gfile, '*'))
+                        for img in images:
+                            shutil.copy(img, os.path.join(target, dir_map))
+
+                        images = [os.path.basename(i) for i in images]
+                        with open(slideshow_html_file, 'wb') as output:
+                            output.write(template.render({'images': images}))
+
+                else:
+                    for k, v in mapping.iteritems():
+                        if gfile.endswith(k):
+                            haskey[k] = True
+                            if not check:
+                                shutil.copy(gfile, os.path.join(target, v))
+                    if not check:
+                        with open(logo_html_file, 'wb') as output:
+                            output.write(logo_template.render({}))
+                        with open(video_html_file, 'wb') as output:
+                            output.write(video_template.render({}))
+
+            if check:
+                if hasdir is False:
+                    print "%s is missing something a subdir" % subdir
+                for k, v in mapping.iteritems():
+                    if haskey[k] is False:
+                        print "%s is missing key %s => %s" % (
+                            subdir, k, v)
+            if not check:
+                break
