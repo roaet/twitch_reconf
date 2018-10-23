1d5a93acefa106162e243237d092c4ee363d47e7
Justin Hammond <justin.hammond@rackspace.com>
2018-10-23 14:09:48 -0500

    Refactored and using logging

--- twitch_reconf/main.py
+++ twitch_reconf/main.py
@@ -1,4 +1,5 @@
 import glob
+import logging
 import os
 import shutil
 import sys
@@ -6,8 +7,171 @@ import sys
 import click
 import jinja2 as j2
 
-import config
-import constants
+from twitch_reconf import config
+from twitch_reconf import constants
+from twitch_reconf import logger as L
+
+
+LOG = logging.getLogger(__name__)
+
+
+class GameConfigurer(object):
+    def __init__(self, debug):
+        self.debug = debug
+
+    def check_target_source_sanity(self, target, source):
+        if source is None or target is None:
+            LOG.error('Source or target is invalid')
+            exit(1)
+        LOG.debug('Copying contents of: %s' % str(source))
+        if not os.path.isdir(target):
+            LOG.error('Source not a directory (%s)' % source)
+            exit(1)
+        LOG.debug('Target directory is: %s' % str(target))
+        if not os.path.isdir(target):
+            LOG.error('Target not a directory (%s)' % target)
+            exit(1)
+
+    def load_template(self, src):
+        with open(src, 'r') as file:
+            return j2.Template(file.read())
+        return None
+
+    def load_directory_info(self):
+        self.target = self.conf.get('TWRECONF', {}).get('target')
+        self.source = self.conf.get('TWRECONF', {}).get('source')
+        self.check_target_source_sanity(self.target, self.source)
+
+        self.dir_map = self.conf.get('TWRECONF', {}).get('dir')
+
+        self.slideshow_dir_src = os.path.join(
+            constants.PROJECT_ROOT, 'slideshow')
+        self.template_dir = os.path.join(
+            constants.PROJECT_ROOT, 'template')
+        self.slideshow_html_src = os.path.join(
+            constants.PROJECT_ROOT, 'template', 'slideshow.j2.html')
+        self.logo_html_src = os.path.join(
+            constants.PROJECT_ROOT, 'template', 'logo.j2.html')
+        self.video_html_src = os.path.join(
+            constants.PROJECT_ROOT, 'template', 'video.j2.html')
+        self.slideshow_imgdir = os.path.join(self.target, self.dir_map, 'img')
+        self.slideshow_html_file = os.path.join(
+            self.target, self.dir_map, 'slideshow.html')
+        self.logo_html_file = os.path.join(self.target, 'logo.html')
+        self.video_html_file = os.path.join(self.target, 'video.html')
+
+    def configure_logging(self):
+        log = L.Logger(self.conf)
+        log.configure()
+
+    def clear_target_dir(self, target):
+        delfiles = glob.glob(os.path.join(target, '*'))
+        for delf in delfiles:
+            if os.path.isdir(delf):
+                shutil.rmtree(delf)
+                LOG.debug('Removed %s' % str(delf))
+            else:
+                os.remove(delf)
+                LOG.debug('Removed %s' % str(delf))
+
+    def load_extension_mappings(self):
+        LOG.debug('Extension Mappings:')
+        self.mapping = {}
+        for k, v in self.conf.get('TWRECONF_EXT', {}).iteritems():
+            self.mapping[k] = v
+            LOG.debug("*.%s = %s" % (k, v))
+
+    def write_template(self, filename, template, conf={}):
+        with open(filename, 'wb') as output:
+            output.write(template.render(conf))
+
+    def get_games_in_source(self, source):
+        files = glob.glob(os.path.join(self.source, '*'))
+        return files
+
+    def initialize(self):
+        self.conf = config.Configuration()
+        for d in self.conf._get_directory_info():
+            LOG.debug(d)
+        self.conf = self.conf.config
+
+        self.configure_logging()
+        self.load_extension_mappings()
+        self.load_directory_info()
+
+    def run_list(self):
+        self.initialize()
+        files = self.get_games_in_source(self.source)
+        game_list = [os.path.basename(f) for f in files]
+        print ', '.join(game_list)
+
+    def run_check(self):
+        self.initialize()
+        files = self.get_games_in_source(self.source)
+        for subdir in files:
+            if(
+                os.path.basename(subdir) in ['Unsorted', 'Default'] or
+                not os.path.isdir(subdir)):
+                continue
+            hasdir = False
+            haskey = {k: False for k in self.mapping.keys()}
+            gamefiles = glob.glob(os.path.join(subdir, '*'))
+            for gfile in gamefiles:  # game specific folder
+                if os.path.isdir(gfile):  # slide files
+                    slide_dir = os.path.basename(gfile)
+                    hasdir = True
+                else:  # files immediately in game folder
+                    for k, v in self.mapping.iteritems():
+                        if gfile.endswith(k):
+                            haskey[k] = True
+
+            if hasdir is False:
+                LOG.warning("%s is missing something a subdir" % subdir)
+            for k, v in self.mapping.iteritems():
+                if haskey[k] is False:
+                    LOG.warning("%s is missing key %s => %s" % (
+                        subdir, k, v))
+
+    def run(self, game):
+        self.initialize()
+
+        template = self.load_template(self.slideshow_html_src)
+        logo_template = self.load_template(self.logo_html_src)
+        video_template = self.load_template(self.video_html_src)
+
+        self.clear_target_dir(self.target)
+        files = self.get_games_in_source(self.source)
+        for subdir in files:
+            if(
+                os.path.basename(subdir) in ['Unsorted', 'Default'] or
+                not os.path.isdir(subdir) or
+                not os.path.basename(subdir) == game):
+                continue
+            gamefiles = glob.glob(os.path.join(subdir, '*'))
+            for gfile in gamefiles:  # game specific folder
+                if os.path.isdir(gfile):  # slide files
+                    slide_dir = os.path.basename(gfile)
+                    # Copy entire slideshow source files
+                    shutil.copytree(
+                        self.slideshow_dir_src,
+                        os.path.join(self.target, self.dir_map))
+                    images = glob.glob(os.path.join(gfile, '*'))
+                    # Copy all sideshow images
+                    for img in images:
+                        shutil.copy(img, os.path.join(
+                            self.target, self.dir_map))
+                    # Render slideshow html
+                    images = [os.path.basename(i) for i in images]
+                    self.write_template(
+                        self.slideshow_html_file, template, {'images': images})
+                    continue
+                # files immediately in game folder
+                for k, v in self.mapping.iteritems():
+                    if gfile.endswith(k):
+                        shutil.copy(gfile, os.path.join(self.target, v))
+                self.write_template(self.logo_html_file, logo_template)
+                self.write_template(self.video_html_file, video_template)
+            break
 
 
 @click.command(context_settings={'ignore_unknown_options': True})
@@ -17,126 +181,11 @@ import constants
 @click.option('--check', is_flag=True)
 @click.argument('game')
 def run(debug, check, game):
-    conf = config.Configuration()
-    for d in conf._get_directory_info():
-        print d
-    mapping = {}
-    list = False
+    configurator = GameConfigurer(debug)
     if game == 'list':
-        list = True
-    print "=" * 75
-
-    print 'Extension Mappings:'
-    for k, v in conf.config.get('TWRECONF_EXT', {}).iteritems():
-        mapping[k] = v
-        print "*.%s = %s" % (k, v)
-    dir_map = conf.config.get('TWRECONF', {}).get('dir')
-    print "Directory Mapping:"
-    print "%s = %s" % ('directory', 'dir_map')
-
-    target = conf.config.get('TWRECONF', {}).get('target')
-    source = conf.config.get('TWRECONF', {}).get('source')
-    if source is None or target is None:
-        print 'Source or target is invalid'
-        exit(1)
-    print 'Copying contents of: %s' % str(source)
-    if not os.path.isdir(target):
-        print 'Source not a directory (%s)' % source
-        exit(1)
-    print 'Target directory is: %s' % str(target)
-    if not os.path.isdir(target):
-        print 'Target not a directory (%s)' % target
-        exit(1)
-
-
-    slideshow_dir_src = os.path.join(constants.PROJECT_ROOT, 'slideshow')
-    template_dir = os.path.join(
-        constants.PROJECT_ROOT, 'template')
-    slideshow_html_src = os.path.join(
-        constants.PROJECT_ROOT, 'template', 'slideshow.j2.html')
-    logo_html_src = os.path.join(
-        constants.PROJECT_ROOT, 'template', 'logo.j2.html')
-    video_html_src = os.path.join(
-        constants.PROJECT_ROOT, 'template', 'video.j2.html')
-    slideshow_imgdir = os.path.join(target, dir_map, 'img')
-    slideshow_html_file = os.path.join(target, dir_map, 'slideshow.html')
-    logo_html_file = os.path.join(target, 'logo.html')
-    video_html_file = os.path.join(target, 'video.html')
-
-    template = None
-    with open(slideshow_html_src, 'r') as file:
-        template = j2.Template(file.read())
-
-    logo_template = None
-    with open(logo_html_src, 'r') as file:
-        logo_template = j2.Template(file.read())
-
-    video_template = None
-    with open(video_html_src, 'r') as file:
-        video_template = j2.Template(file.read())
-
-    print "-" * 75
-    if not check and not list:
-        delfiles = glob.glob(os.path.join(target, '*'))
-        for delf in delfiles:
-            if os.path.isdir(delf):
-                shutil.rmtree(delf)
-                print 'Removed %s' % str(delf)
-            else:
-                os.remove(delf)
-                print 'Removed %s' % str(delf)
-
-    print "+" * 75
-    files = glob.glob(os.path.join(source, '*'))
-    if list:
-        game_list = [os.path.basename(f) for f in files]
-        print ', '.join(game_list)
+        configurator.run_list()
         exit(0)
-
-    for subdir in files:
-        hasdir = False
-        haskey = {k: False for k in mapping.keys()}
-        if(
-                os.path.isdir(subdir) and 
-                (os.path.basename(subdir) == game or check)):
-            if str(os.path.basename(subdir)) in ['Unsorted', 'Default']:
-                continue
-
-            gamefiles = glob.glob(os.path.join(subdir, '*'))
-            for gfile in gamefiles:
-                if os.path.isdir(gfile):
-                    slide_dir = os.path.basename(gfile)
-                    hasdir = True
-                    if not check:
-                        shutil.copytree(
-                            slideshow_dir_src,
-                            os.path.join(target, dir_map))
-                        images = glob.glob(os.path.join(gfile, '*'))
-                        for img in images:
-                            shutil.copy(img, os.path.join(target, dir_map))
-
-                        images = [os.path.basename(i) for i in images]
-                        with open(slideshow_html_file, 'wb') as output:
-                            output.write(template.render({'images': images}))
-
-                else:
-                    for k, v in mapping.iteritems():
-                        if gfile.endswith(k):
-                            haskey[k] = True
-                            if not check:
-                                shutil.copy(gfile, os.path.join(target, v))
-                    if not check:
-                        with open(logo_html_file, 'wb') as output:
-                            output.write(logo_template.render({}))
-                        with open(video_html_file, 'wb') as output:
-                            output.write(video_template.render({}))
-
-            if check:
-                if hasdir is False:
-                    print "%s is missing something a subdir" % subdir
-                for k, v in mapping.iteritems():
-                    if haskey[k] is False:
-                        print "%s is missing key %s => %s" % (
-                            subdir, k, v)
-            if not check:
-                break
+    if game == 'check':
+        configurator.run_check()
+        exit(0)
+    configurator.run(game)
